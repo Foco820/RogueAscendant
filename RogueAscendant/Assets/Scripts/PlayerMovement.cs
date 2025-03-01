d4ea24f3b9b92d275bc22ecbee7c2308bf05fbe8
Foco820 <2156914087@qq.com>
2025-03-01 15:59:48 +0800

    实现简单移动和转向
    使用新输入系统的事件驱动简单实现了人物的前后移动和左右转向

--- RogueAscendant/Assets/Scripts/PlayerMovement.cs
+++ RogueAscendant/Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float rotationSpeed = 120f;
     [SerializeField] float jumpForce;
 
     Rigidbody rb;
+
+    InputControl control;
+
+    float moveInput;
+    float rotationInput;
 
     private void Start()
     {
@@ -19,7 +25,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        
+        transform.position += transform.forward * moveInput * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, rotationInput * rotationSpeed * Time.deltaTime, 0);
     }
 
     public void Jump(InputAction.CallbackContext ctx)
@@ -28,7 +35,24 @@ public class PlayerMovement : MonoBehaviour
         {
             Debug.Log("飞起来！");
             rb.velocity += Vector3.up * jumpForce;
-            rb.velocity += Vector3.right * jumpForce;
+        }
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        if (ctx.phase == InputActionPhase.Performed)
+        {
+            Debug.Log("动起来！");
+            moveInput = ctx.ReadValue<float>();
+        }
+    }
+
+    public void Rotate(InputAction.CallbackContext ctx)
+    {
+        if (ctx.phase == InputActionPhase.Performed)
+        {
+            Debug.Log("转起来了");
+            rotationInput = ctx.ReadValue<float>();
         }
     }
 }
